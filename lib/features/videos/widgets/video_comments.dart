750e6b4b7d0fc5bdb2f32183d049818db702280f
jiho <ghrms80@gmail.com>
2023-05-12 08:19:30 +0900

    8.1 Comments

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -33,10 +33,72 @@ class _VideoCommentsState extends State<VideoComments> {
             ),
           ],
         ),
-        body: ListView.builder(
+        body: ListView.separated(
+          padding: const EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 16,
+          ),
+          separatorBuilder: (context, index) {
+            return const SizedBox(height: 20);
+          },
           itemCount: 10,
-          itemBuilder: (context, index) => Container(
-            child: const Text('I\'m a comment'),
+          itemBuilder: (context, index) => Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CircleAvatar(
+                radius: 18,
+                child: Text('니꼬'),
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '니꼬',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                    const SizedBox(width: 3),
+                    const Text(
+                        "That's not it live seen the same thing but also in a cave,"),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 10),
+              Column(
+                children: [
+                  Icon(
+                    Icons.favorite_border,
+                    color: Colors.grey.shade500,
+                    size: 20,
+                  ),
+                  const SizedBox(height: 2),
+                  Text(
+                    '52.2K',
+                    style: TextStyle(
+                      color: Colors.grey.shade500,
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 18,
+                backgroundColor: Colors.grey.shade500,
+                foregroundColor: Colors.white,
+                child: const Text('니꼬'),
+              ),
+            ],
           ),
         ),
       ),
