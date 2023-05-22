b5f307c996fc95823f3de5c8583299a741bb767e
jiho <ghrms80@gmail.com>
2023-05-22 14:36:04 +0900

    12.2 SliverAppBar

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -4,6 +4,7 @@ import 'package:tiktoc_clne_re0/features/discover/discover_screen.dart';
 import 'package:tiktoc_clne_re0/features/inbox/inbox_screen.dart';
 import 'package:tiktoc_clne_re0/features/main_navigation/widgets/nav_tab.dart';
 import 'package:tiktoc_clne_re0/features/main_navigation/widgets/post_video_button.dart';
+import 'package:tiktoc_clne_re0/features/users/user_profile_screen.dart';
 import 'package:tiktoc_clne_re0/features/videos/video_timeline_screen.dart';
 
 class MainNavigationScreen extends StatefulWidget {
@@ -14,7 +15,7 @@ class MainNavigationScreen extends StatefulWidget {
 }
 
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
-  int _selectedIndex = 3;
+  int _selectedIndex = 4;
 
   void _onTap(int index) {
     setState(() {
@@ -58,7 +59,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
           ),
           Offstage(
             offstage: _selectedIndex != 4,
-            child: Container(),
+            child: const UserProfileScreen(),
           ),
         ],
       ),
