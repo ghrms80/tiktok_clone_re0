b10915e3a65b820e761bb09b32dd2d582180757d
jiho <ghrms80@gmail.com>
2023-05-24 09:13:59 +0900

    13.4 CupertinoAlertDialog

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,7 +1,22 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _notifications = false;
+
+  void _onNotificationsChanged(bool? newValue) {
+    if (newValue == null) return;
+    setState(() {
+      _notifications = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,9 +25,80 @@ class SettingsScreen extends StatelessWidget {
         title: const Text("Settings"),
         centerTitle: true,
       ),
-      body: Column(
-        children: const [
-          CloseButton(),
+      body: ListView(
+        children: [
+          CupertinoSwitch(
+            value: _notifications,
+            onChanged: _onNotificationsChanged,
+          ),
+          Switch(
+            value: _notifications,
+            onChanged: _onNotificationsChanged,
+          ),
+          Switch.adaptive(
+            value: _notifications,
+            onChanged: _onNotificationsChanged,
+          ),
+          Checkbox(
+            value: _notifications,
+            onChanged: _onNotificationsChanged,
+          ),
+          SwitchListTile(
+            value: _notifications,
+            onChanged: _onNotificationsChanged,
+            title: const Text("Enable notifications"),
+            subtitle: const Text("Enable notifications"),
+          ),
+          CheckboxListTile(
+            value: _notifications,
+            onChanged: _onNotificationsChanged,
+            title: const Text("Enable notifications"),
+            subtitle: const Text("Enable notifications"),
+            activeColor: Colors.black,
+          ),
+          const AboutListTile(
+            // add
+            applicationName: "TikTok Clone",
+            applicationVersion: "1.0",
+            applicationLegalese: "All rights reserved. Please don't copy me.",
+          ),
+          ListTile(
+            onTap: () async {
+              final date = await showDatePicker(
+                // return future
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1960),
+                lastDate: DateTime(2040),
+              );
+              print(date);
+              final time = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+              print(time);
+
+              // ignore: use_build_context_synchronously
+              final booking = await showDateRangePicker(
+                context: context,
+                firstDate: DateTime(1970),
+                lastDate: DateTime(2040),
+                builder: (context, child) {
+                  return Theme(
+                    data: ThemeData(
+                      appBarTheme: const AppBarTheme(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.black,
+                      ),
+                    ),
+                    child: child!, // 자신에게 Theme을 적용
+                  );
+                },
+              );
+              print(booking);
+            },
+            title: const Text("What is your birthday?"),
+          ),
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tiktoc_clne_re0/features/main_navigation/main_navigation_screen.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -33,6 +34,8 @@ class TikTokApp extends StatelessWidget {
         ),
       ),
       home: const MainNavigationScreen(),
+      localizationsDelegates: const [GlobalMaterialLocalizations.delegate],
+      supportedLocales: const [Locale('en'), Locale('ko')],
     );
   }
 }
